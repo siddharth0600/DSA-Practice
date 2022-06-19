1ed9480f51e2fb16f1d48ce1878b86afb4a41c1f
Siddharth Tripathi <52525767+siddharth0600@users.noreply.github.com>
2022-06-19 21:42:25 +0530

    Time: 3 ms (96.73%), Space: 8 MB (47.66%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -22,9 +22,9 @@ public:
         for(int i=0;i<=n;i++){
             
             if(i%2==0) 
-                ans[i] = ans[i/2];
+                ans[i] = ans[i/2];//For even nos
             else
-                ans[i] = ans[i/2] +1;
+                ans[i] = ans[i/2] +1; // For odd nos
         }
         
         return ans;
