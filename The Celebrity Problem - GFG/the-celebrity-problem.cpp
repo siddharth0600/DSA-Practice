9845dbb38ef1305c8a4b56ce337825ca67cec7cf
Siddharth Tripathi <52525767+siddharth0600@users.noreply.github.com>
2022-06-26 01:23:34 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -15,11 +15,12 @@ class Solution
     {
         // code here 
         stack<int> s1;
-        
+        // Pushed all the elements into the stack
         for(int i=0;i<n;i++){
             s1.push(i);
         }
         
+        //Now the loop will run until the size is equal to 1
         while(s1.size()!=1){
             int a=s1.top();
             s1.pop();
@@ -31,11 +32,16 @@ class Solution
             else if(M[b][a]==1) s1.push(a);
         }
         
+        // "can" element is the potential candidarte yo become celebrity
+        
+        //first checking the row of the can whether all are 0 if that means
+        // can doen't know anyone
         int can=s1.top();
         for(int i=0;i<n;i++){
             if(M[can][i]==1) return -1;
         }
         
+        //now checking if can is known by everyone that means whole column is 1 except where can=can
         for(int i=0;i<n;i++){
             if(i==can) continue;
             if(M[i][can]==0) return -1;
