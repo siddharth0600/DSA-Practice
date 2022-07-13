8b739be18c741f8461ed2154ce959e474dd55e0e
Siddharth Tripathi <52525767+siddharth0600@users.noreply.github.com>
2022-07-13 11:47:27 +0530

    Time: 12 ms (14.03%), Space: 9.6 MB (5.10%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,23 +1,57 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int tarRow=0;
-        int n=matrix.size();
-        int m=matrix[0].size();
+//         if(matrix.size()==0) return false;
         
-        for(int i=0;i<matrix.size();i++){
-            if(target>=matrix[i][0] && target<=matrix[i][m-1]){
-                tarRow=i;
-                break;
+       
+//         int mid=0;
+//         int iin,jin;
+//         int n=matrix.size();
+//         int m=matrix[0].size();
+        
+//         int i=0;
+//         int j=n*m-1;
+        
+//         while(i<=j){
+            
+//             mid=(i+(j-i)/2);
+//             iin = mid/n;
+//             jin=mid%m;
+//             if(matrix[iin][jin]==target) return true;
+            
+//             if(matrix[iin][jin]>target)
+//                 j=mid-1;
+//             else
+//                 i=mid+1;
+                    
+            
+//         }
+//         return false;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int start = 0;
+        int end  = row*col-1;
+        
+        int mid = start + (end-start)/2;
+        
+        while(start<=end) {
+            
+            int element = matrix[mid/col][mid%col];
+            
+            if(element == target) {
+                return 1;
             }
+            
+            if(element < target) {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+             mid = start + (end-start)/2;    
         }
-        
-        for(int j=0;j<m;j++){
-            if(matrix[tarRow][j]==target){
-                return true;
-            }    
-        }
-        return false;
-        
+        return 0;
     }
 };
